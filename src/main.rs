12bbddfc6f942023b3000a32d1ff20820535438c
Sean Kelly <code@katron.org>
2017-05-12 21:45:09 -0500

    Start very basic version with just peak age
    The peak age is a single, simple variable.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("Capuchin")
+        .version("0.1.0")
+        .about("Simple baseball projections")
+        .arg(Arg::with_name("year")
+             .short("y")
+             .long("year")
+             .value_name("YEAR")
+             .help("Year to project")
+             .takes_value(true))
+        .arg(Arg::with_name("peak_age")
+             .short("a")
+             .long("peak-age")
+             .value_name("AGE")
+             .help("Peak age for player")
+             .takes_value(true))
+        ;
+    let matches = app.get_matches();
 }
