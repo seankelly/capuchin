e32e11a3c10854b18b8d32e0ec76683a5e09e306
Sean Kelly <code@katron.org>
2017-05-15 22:18:50 -0500

    Add enough code to parse Batting.csv

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ extern crate clap;
 extern crate csv;
 #[macro_use]
 extern crate error_chain;
+extern crate rustc_serialize;
 
 use clap::{Arg, App};
+use std::path::Path;
 use std::default::Default;
 
 mod errors {
-    error_chain! {}
+    use csv;
+
+    error_chain! {
+        foreign_links {
+            Io(csv::Error);
+        }
+    }
 }
 
 struct ProjectionOptions {
@@ -17,6 +25,7 @@ struct ProjectionOptions {
     year_weights: Vec<f32>,
 }
 
+#[derive(RustcDecodable)]
 struct BattingSeason {
     playerid: String,
     yearid: u16,
@@ -30,16 +39,16 @@ struct BattingSeason {
     double: u8,
     triple: u8,
     hr: u8,
-    rbi: u8,
-    sb: u8,
-    cs: u8,
+    rbi: Option<u8>,
+    sb: Option<u8>,
+    cs: Option<u8>,
     bb: u16,
-    so: u16,
-    ibb: u8,
-    hbp: u8,
-    sh: u8,
-    sf: u8,
-    gidp: u8,
+    so: Option<u16>,
+    ibb: Option<u8>,
+    hbp: Option<u8>,
+    sh: Option<u8>,
+    sf: Option<u8>,
+    gidp: Option<u8>,
 }
 
 impl Default for ProjectionOptions {
@@ -56,6 +65,13 @@ fn main() {
     let app = App::new("Capuchin")
         .version("0.1.0")
         .about("Simple baseball projections")
+        .arg(Arg::with_name("batting")
+             .short("b")
+             .long("batting")
+             .value_name("FILE")
+             .required(true)
+             .help("Batting.csv file")
+             .takes_value(true))
         .arg(Arg::with_name("year")
              .short("y")
              .long("year")
@@ -70,4 +86,18 @@ fn main() {
              .takes_value(true))
         ;
     let matches = app.get_matches();
+
+    let batting_csv = Path::new(matches.value_of("batting").expect("No Batting.csv file."));
+    let batting = load_batting_season(batting_csv);
+}
+
+fn load_batting_season(batting_csv: &Path) -> errors::Result<()> {
+    let mut rdr = csv::Reader::from_file(batting_csv)?;
+    let mut found_records = 0;
+    for record in rdr.decode() {
+        let record: BattingSeason = record?;
+        found_records += 1;
+    }
+    println!("Found {} records", found_records);
+    Ok(())
 }
