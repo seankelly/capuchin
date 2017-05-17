b7f5c047e4d4e4c5b67b29ec3b76cc90063d5ff6
Sean Kelly <code@katron.org>
2017-05-16 21:44:00 -0500

    Rename ProjectionOptions to be the proper Projection

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod errors {
     }
 }
 
-struct ProjectionOptions {
+struct Projection {
     peak_age: u16,
     year_weights: Vec<f32>,
 }
@@ -93,9 +93,9 @@ fn load_batting_season(batting_csv: &Path) -> errors::Result<()> {
     Ok(())
 }
 
-impl Default for ProjectionOptions {
+impl Default for Projection {
     fn default() -> Self {
-        ProjectionOptions {
+        Projection {
             peak_age: 29,
             year_weights: vec![5.0, 4.0, 3.0],
         }
